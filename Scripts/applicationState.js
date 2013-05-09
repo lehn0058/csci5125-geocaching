693615e0114d4e5d5b562046003cf2977b723e4b
Brandon Lehner <lehner.brandon@gmail.com>
2013-05-08 21:08:39 -0500

    fixing bad merge

--- Scripts/applicationState.js
+++ Scripts/applicationState.js
@@ -20,14 +20,14 @@
 $.applicationState = {
     // The name of the user
     userName: "John Doe",
-    
+
     // The current geocache that a user has selected. Defaults to null since a user doesn't initially have one selected.
     selectedGeocache: null,
 
     // The current geocaching reservation that a user has selected. Defaults to null since a user doesn't initially have one selected.
     selectedGCReservation: null,
-    
-     // The current regular reservation that a user has selected. Defaults to null since a user doesn't initially have one selected.
+
+    // The current regular reservation that a user has selected. Defaults to null since a user doesn't initially have one selected.
     selectedRegReservation: null,
 
     // Filters related to what caches should be displayed
@@ -39,7 +39,7 @@ $.applicationState = {
 
     // A few collections of our mocked up geocache locations
     allGeocaches: [
-        { id: 1, lat: 45.478652, lon: -92.754652, name: "My First GeoCache", group: $.enums.cacheGroups.nearYou, lastFound: new Date(2012, 11, 15, 5, 0, 0, 0), difficulty: $.enums.difficulty.easy, foundBy: [{ name: "John D." }, { name: "Ellen M." }, { name: "Mike E." }], hints: [{ name: "Make sure to look closely at all the leaves."} ] },
+        { id: 1, lat: 45.478652, lon: -92.754652, name: "My First GeoCache", group: $.enums.cacheGroups.nearYou, lastFound: new Date(2012, 11, 15, 5, 0, 0, 0), difficulty: $.enums.difficulty.easy, foundBy: [{ name: "John D." }, { name: "Ellen M." }, { name: "Mike E." }], hints: [{ name: "Make sure to look closely at all the leaves." }] },
         { id: 2, lat: 45.578652, lon: -92.654652, name: "By the pond", group: $.enums.cacheGroups.nearYou, lastFound: new Date(2013, 3, 3, 12, 0, 0, 0), difficulty: $.enums.difficulty.normal, foundBy: [{ name: "John D." }], hints: [{ name: "This one should probably be a tiny size instead of a small. I was not looking close enough at first." }] },
         { id: 3, lat: 45.678652, lon: -93.354652, name: "Near the forrest", group: $.enums.cacheGroups.nearYou, lastFound: new Date(2012, 11, 15, 5, 0, 0, 0), difficulty: $.enums.difficulty.hard, foundBy: [{ name: "Zach Q." }, { name: "Ellorie L." }, { name: "Josh J." }, { name: "Quinton T." }], hints: [{ name: "Look near the trees, but NOT in them." }] },
         { id: 4, lat: 44.778652, lon: -93.254652, name: "Through the woods", group: $.enums.cacheGroups.nearYou, lastFound: new Date(2012, 10, 23, 5, 0, 0, 0), difficulty: $.enums.difficulty.hard, foundBy: [{ name: "Ellen M." }, { name: "Mike E." }], hints: [{ name: "Going with someone tall makes this one much easier." }] },
@@ -50,20 +50,20 @@ $.applicationState = {
 
     // A few collections of our mocked up gcReservations
     allGCReservations: [
-        { id: 1, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 6, 6, 8, 0, 0, 0), difficulty: $.enums.difficulty.hard, equipmentsSupport: [{ name: "Magellan CX0310SGXNA eXplorist 310 Waterproof Hiking GPS" }, { name: "Cache Advance Magnetic Bolt" }, { name: "Garmin nüvi 1300T GPS Navigation System" }], foodSupport: [{ name: "Cookies"},{ name: "Bread"},{ name: "AppleJuice"} ], contact: "6122345612" },
-        { id: 2, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 7, 1, 12, 0, 0, 0), difficulty: $.enums.difficulty.normal, equipmentsSupport: [{ name: "Garmin nüvi 1300T GPS Navigation System" }], foodSupport: [{ name: "OrangeJuice" },{ name: "ChickenWings" }], contact: "6122345612" },
-        { id: 3, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 5, 31, 9, 0, 0, 0), difficulty: $.enums.difficulty.hard, equipmentsSupport: [{ name: "Cache Advance Magnetic Bolt" }, { name: "Cache Advance Magnetic Vehicle Travel Bug" }, { name: "Lowrance Endura Outback Gps" }], foodSupport: [{ name: "AppleJuice" },{ name: "Sandwich" }] , contact: "6122345612"},
+        { id: 1, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 6, 6, 8, 0, 0, 0), difficulty: $.enums.difficulty.hard, equipmentsSupport: [{ name: "Magellan CX0310SGXNA eXplorist 310 Waterproof Hiking GPS" }, { name: "Cache Advance Magnetic Bolt" }, { name: "Garmin nüvi 1300T GPS Navigation System" }], foodSupport: [{ name: "Cookies" }, { name: "Bread" }, { name: "AppleJuice" }], contact: "6122345612" },
+        { id: 2, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 7, 1, 12, 0, 0, 0), difficulty: $.enums.difficulty.normal, equipmentsSupport: [{ name: "Garmin nüvi 1300T GPS Navigation System" }], foodSupport: [{ name: "OrangeJuice" }, { name: "ChickenWings" }], contact: "6122345612" },
+        { id: 3, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, cachingDate: new Date(2013, 5, 31, 9, 0, 0, 0), difficulty: $.enums.difficulty.hard, equipmentsSupport: [{ name: "Cache Advance Magnetic Bolt" }, { name: "Cache Advance Magnetic Vehicle Travel Bug" }, { name: "Lowrance Endura Outback Gps" }], foodSupport: [{ name: "AppleJuice" }, { name: "Sandwich" }], contact: "6122345612" },
         { id: 4, organizer: "Divid", group: $.enums.eventCategories.appliedByOthers, cachingDate: new Date(2013, 10, 19, 10, 0, 0, 0), difficulty: $.enums.difficulty.easy, equipmentsSupport: [{ name: "Lowrance Endura Outback Gps" }], foodSupport: [{ name: "OrangeJuice" }], contact: "2119087761" },
-        { id: 5, organizer: "Jessica", group: $.enums.eventCategories.appliedByOthers, cachingDate: new Date(2013, 5, 22, 14, 0, 0, 0), difficulty: $.enums.difficulty.easy, equipmentsSupport: [{ name: "Cache Angel Geocoin - The Seeker - Antique Silver" }], foodSupport: [{ name: "Sandwich" }, { name: "Cookies" }] , contact: "803712467"}
+        { id: 5, organizer: "Jessica", group: $.enums.eventCategories.appliedByOthers, cachingDate: new Date(2013, 5, 22, 14, 0, 0, 0), difficulty: $.enums.difficulty.easy, equipmentsSupport: [{ name: "Cache Angel Geocoin - The Seeker - Antique Silver" }], foodSupport: [{ name: "Sandwich" }, { name: "Cookies" }], contact: "803712467" }
     ],
 
     // A few collections of our mocked up regReservations
     allRegReservations: [
         { id: 1, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, date: new Date(2013, 6, 1, 8, 0, 0, 0), locating: "Wilson Park, Philadelphia, PA", theme: "public lecture", contact: "6122345612" },
         { id: 2, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, date: new Date(2013, 6, 8, 10, 0, 0, 0), locating: "El Taller Del Alfarero Center, 14521 South Normandie AvenueGardena, CA", theme: "happy weekend dinner", contact: "6122345612" },
-        { id: 3, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, date: new Date(2013, 7, 1, 10, 0, 0, 0), locating: "Chianti Grill, 2050 N Snelling Ave, Roseville, MN", theme: "annual geocaching meeting for local group", contact: "6122345612"},
+        { id: 3, organizer: "John Doe", group: $.enums.eventCategories.appliedByMe, date: new Date(2013, 7, 1, 10, 0, 0, 0), locating: "Chianti Grill, 2050 N Snelling Ave, Roseville, MN", theme: "annual geocaching meeting for local group", contact: "6122345612" },
         { id: 4, organizer: "Divid", group: $.enums.eventCategories.appliedByOthers, date: new Date(2013, 7, 2, 9, 30, 0, 0), locating: "New York University, 44 W 4th St(between Greene St & Mercer St) New York, NY ", theme: "national geocaching knowledge competition", contact: "2119087761" },
-        { id: 5, organizer: "Jessica", group: $.enums.eventCategories.appliedByOthers, date: new Date(2013, 5, 22, 14, 30, 0, 0), locating: "The Original Valentino's, 33497 S Dixie Hwy Unit 105 Florida City, FL  ", theme: "local flee market activity for geocaching device ", contact: "803712467"}
+        { id: 5, organizer: "Jessica", group: $.enums.eventCategories.appliedByOthers, date: new Date(2013, 5, 22, 14, 30, 0, 0), locating: "The Original Valentino's, 33497 S Dixie Hwy Unit 105 Florida City, FL  ", theme: "local flee market activity for geocaching device ", contact: "803712467" }
     ],
 
 
@@ -75,7 +75,7 @@ $.applicationState = {
         $('#user-settings').live('pageinit', function (event) {
             $.applicationState.initUserSettings();
         });
-        
+
         $('#user-reservations').live('pageinit', function (event) {
             $.applicationState.initUserReservations();
         });
@@ -83,7 +83,7 @@ $.applicationState = {
         $('#gcReservation-detail').live('pageinit', function (event) {
             $.applicationState.initGCReservationDetail();
         });
-        
+
         $('#regReservation-detail').live('pageinit', function (event) {
             $.applicationState.initRegReservationDetail();
         });
@@ -143,8 +143,8 @@ $.applicationState = {
 
     // Initialize the user-resverations.html page
     initUserReservations: function () {
-       
-            // Add all reservations to the list view in their appropriate section
+
+        // Add all reservations to the list view in their appropriate section
         this.addSectionHeader($("#gcReservations-collection"), "GeoCaching 101 Events");
         this.addSelectableItems2($("#gcReservations-collection"), $.applicationState.allGCReservations);
         this.addSectionHeader($("#regReservations-collection"), "Applied by Others");
@@ -156,17 +156,17 @@ $.applicationState = {
             if (id) {
                 var idInt = parseInt(id);
                 var item = $.grep($.applicationState.allReservations, function (item, index) { return item.id == idInt; })[0];
-                $.applicationState.selectedGCReservation= item;
+                $.applicationState.selectedGCReservation = item;
             }
         });
-        
+
         // Attach an event listener to each item in the collection
         $('#regReservations-collection li').on('click', function () {
             var id = $("a", this).attr("regRes-id");
             if (id) {
                 var idInt = parseInt(id);
                 var item = $.grep($.applicationState.allReservations, function (item, index) { return item.id == idInt; })[0];
-                $.applicationState.selectedRegReservation= item;
+                $.applicationState.selectedRegReservation = item;
             }
         });
     },
@@ -180,7 +180,7 @@ $.applicationState = {
         $("#sponsored-food-count").text(this.selectedGCReservation.foodSupport.length);
         $("#contact-info").text(this.selectedGCReservation.contact);
     },
-    
+
     // Initializes the regReservation-detail.html page
     initGCReservationDetail: function () {
         $("#organizer-name").text(this.selectedRegReservation.organizer);
@@ -259,6 +259,7 @@ $.applicationState = {
         $("#target-lon").text(this.selectedGeocache.lon);
 
         this.startTrackPosition();
+        this.drawCompass();
     },
 
     // Adds a section header to a list view.
@@ -274,12 +275,11 @@ $.applicationState = {
             collectionView.append(template).listview('refresh');
         });
     },
-
-
- // Adds a collection of items to a list view(reservation list). These items are selectable.
+    
+    // Adds a collection of items to a list view(reservation list). These items are selectable.
     addSelectableItems2: function (collectionView, items) {
         $.each(items, function (index, item) {
-            var template = '<li data-theme="c"><a href="reservation-detail.html" data-transition="slide" res-id=' + item.id + '>' + item.organizer+ '</a></li>';
+            var template = '<li data-theme="c"><a href="reservation-detail.html" data-transition="slide" res-id=' + item.id + '>' + item.organizer + '</a></li>';
             collectionView.append(template).listview('refresh');
         });
     },
@@ -320,15 +320,77 @@ $.applicationState = {
 
     // Finds the distance between two geo-coordinate positions.
     distanceBetweenCoordinates: function (lat1, lon1, lat2, lon2) {
-        var R = 6371; // km
-        var dLat = (lat2 - lat1).toRad();
-        var dLon = (lon2 - lon1).toRad();
-        var lat1 = lat1.toRad();
-        var lat2 = lat2.toRad();
+        var R = 3959; // mi
+
+        var dLat = this.toRadians(lat2 - lat1);
+        var dLon = this.toRadians(lon2 - lon1);
+        var lat1 = this.toRadians(lat1);
+        var lat2 = this.toRadians(lat2);
 
         var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
                 Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         var d = R * c;
+
+        // Return the distance, in miles, with 2 decimal places.
+        return this.trimDecimal(d);
+    },
+
+    // Trims a decimal number so it only has 2 decimal places.
+    trimDecimal: function (decimal) {
+        return Math.floor(decimal * 100) / 100
+    },
+
+    // Converts degrees to radians
+    toRadians: function (degrees) {
+        return degrees * Math.PI / 180
+    },
+
+    drawCompass: function () {
+        var compass = document.getElementById('compass').appendChild(document.createElement('article'));;
+        compass.id = 'compass';
+        var spinner = compass.appendChild(document.createElement('article'));
+        spinner.id = 'spinner';
+        var pin = spinner.appendChild(document.createElement('article'));
+        pin.id = 'pin';
+
+        for (var degrees = 0, degree; degrees < 360; degrees += 30) {
+            degree = spinner.appendChild(document.createElement('label'));
+            degree.className = 'degree';
+            degree.innerText = degrees;
+            degree.style.webkitTransform = 'rotateZ(' + degrees + 'deg)'
+        }
+        ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'].forEach(function (label, index) {
+            var main = ((index % 2) ? '' : ' main');
+            point = spinner.appendChild(document.createElement('label'));
+            point.className = 'point' + main;
+            point.innerText = label;
+            point.style.webkitTransform = 'rotateZ(' + (index * 45) + 'deg)'
+            arrow = spinner.appendChild(document.createElement('div'));
+            arrow.className = 'arrow' + main;
+            arrow.style.webkitTransform = 'rotateZ(' + (index * 45) + 'deg)'
+        });
+
+        var lastHeading = 0;
+        window.addEventListener('deviceorientation', function (e) {
+            var heading = e.webkitCompassHeading + window.orientation;
+            if (Math.abs(heading - lastHeading) < 180) {
+                spinner.style.webkitTransition = 'all 0.2s ease-in-out';
+            } else {
+                spinner.style.webkitTransition = 'none';
+            }
+            spinner.style.webkitTransform = 'rotateZ(-' + heading + 'deg)';
+            lastHeading = heading;
+        }, false);
+
+        document.body.addEventListener('touchstart', function (e) {
+            e.preventDefault();
+        }, false);
+
+        window.addEventListener('orientationchange', function (e) {
+            window.scrollTo(0, 1);
+        }, false);
+
+        setTimeout(function () { window.scrollTo(0, 1); }, 0);
     }
 }
